04a683c4fe116a36a77d7b7914bd65b87bbb82d7
Bulent Kucuk <bulent.kucuk@uk.ibm.com>
2022-05-14 18:06:35 +0100

    Deploy Next.js to gh-pages

--- _next/static/GV4KaS7nkImN4nH2e0fIh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-571e7412970a92c1.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-274c89abbd5a0fc9.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
