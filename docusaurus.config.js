4be98a46be594e8e10733a67b58f61e5642cf237
Bulent Kucuk <bkucuk@gmail.com>
2023-08-21 11:18:16 +0100

    Fixed error in tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Fisebil',
-  tagline: 'Let's race to do good',
+  tagline: 'Let\'s race to do good',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
