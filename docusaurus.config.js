c7c10eb4ef290083c9c0f8ee783caac3917fb32a
Bulent Kucuk <bkucuk@gmail.com>
2025-01-26 21:25:13 +0000

    Title change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Fi-Sebil',
-  tagline: 'Fi-Sebilillah',
+  title: 'Fî Sebil',
+  tagline: 'Fî Sebilillah',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
